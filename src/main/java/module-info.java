88b6b6e4151a5972bb45961342faae6e51a825f4
Siebrenvde <siebren@siebrenvde.dev>
2025-09-24 21:33:53 +0200

    Remove root package export
    I have an excluded test class there, which is why it built locally

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module dev.siebrenvde.ntfy {
     requires transitive java.net.http;
     requires com.google.gson;
 
-    exports dev.siebrenvde.ntfy;
     exports dev.siebrenvde.ntfy.message;
     exports dev.siebrenvde.ntfy.message.action;
     exports dev.siebrenvde.ntfy.message.attachment;
