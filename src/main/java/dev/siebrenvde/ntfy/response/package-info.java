3c96c957801aa70ea818ae9639b9b327979d434e
Siebrenvde <siebren@siebrenvde.dev>
2025-10-10 18:59:36 +0200

    Add NullMarked package-info files for improved Kotlin support
    All non-null types were considered platform types

---
+++ src/main/java/dev/siebrenvde/ntfy/response/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.siebrenvde.ntfy.response;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/dev/siebrenvde/ntfy/util/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.siebrenvde.ntfy.util;
+
+import org.jspecify.annotations.NullMarked;
